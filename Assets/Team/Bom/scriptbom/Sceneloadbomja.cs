d9b8fe98e82349fe6078296970c92dc1ef1dfc2a
Mr.Bom <bombeamlazer@gmail.com>
2025-10-25 23:54:59 +0700

    startscene

--- Assets/Team/Bom/scriptbom/Sceneloadbomja.cs
+++ Assets/Team/Bom/scriptbom/Sceneloadbomja.cs
@@ -5,8 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class Sceneloadbomja : MonoBehaviour
 {
+
     public void LoadScene(string sceneName)
     {
+        Debug.Log("Loading scene: " + sceneName);
         SceneManager.LoadScene(sceneName);
+        
     }
 }
